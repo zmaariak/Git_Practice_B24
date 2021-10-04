6a17186b79eb678ec0a127fc496534d46bbf2435
maariakothawala <zmaariak@gmail.com>
2021-10-04 14:00:17 -0500

    made the main method

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -12,5 +12,11 @@ public class Third {
 
     public static void main(String[] args) {
         System.out.println("5");
+        m();
+    }
+
+    public static void m(){
+        System.out.println(6);
+
     }
 }
