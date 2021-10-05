534465c0c93ebaa709a2058477bcd1a3d48553ea
maariakothawala <zmaariak@gmail.com>
2021-10-05 12:00:49 -0500

    updating hello world class

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -3,5 +3,7 @@ package tests;
 public class HelloWorldTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("One more hello world");
     }
 }
