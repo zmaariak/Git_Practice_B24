0c4b508685a9c94e4bc7d8c65b7f1bd6c06b6e2e
maariakothawala <zmaariak@gmail.com>
2021-10-05 11:49:23 -0500

    added woodenspoon test

---
+++ src/tests/WoodenSpoonTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class WoodenSpoonTest {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Wooden Spoon");
+
+        }
+    }
+}
